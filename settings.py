31a61add972a04bef82ff1570c10080d177d2f15
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2023-01-12 20:19:14 +0000

    added settings for alien class

--- settings.py
+++ settings.py
@@ -10,10 +10,17 @@ class Settings:
 
         # Ship settings
         self.ship_speed = 2.5
+        self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed = 4.0
+        self.bullet_speed = 5.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # Fleet_direction of 1 represent right; -1 represents left.
+        self.fleet_direction = 1
