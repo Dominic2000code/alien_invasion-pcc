28c2574a63cac420fb1037ab938501ae08ba6bff
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2023-01-12 20:19:26 +0000

    added game statistics class

---
+++ game_stats.py
@@ -0,0 +1,11 @@
+class GameStats:
+    """Track statistics for Alien Invasion"""
+
+    def __init__(self, ai_game) -> None:
+        """Initialize statistics."""
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+    def reset_stats(self):
+        """Initialize statistics that can change during the game."""
+        self.ship_lefts = self.settings.ship_limit
