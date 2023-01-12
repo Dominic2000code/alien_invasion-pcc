8b148ad2568c70b830c817ffe5f8d69b82cc554c
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2023-01-12 09:21:49 +0000

    initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Alien Invasion
+
+The goal of the game is to shoot down a fleet of aliens as they drop down the
+screen, in levels that increase in speed and difficulty.
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A class to store all settings for Alien Invasion"""
+
+    def __init__(self) -> None:
+        """Initialize the gam's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
